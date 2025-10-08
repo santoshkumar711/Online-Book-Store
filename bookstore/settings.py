42f20351f1440f6e57524a6e7669735e988c4841
santoshkumar711 <santoshkumar711711@gmail.com>
2025-10-08 18:25:55 +0530

    settings.py change

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = '#*k)9t8vzu)4j2hr=l@)emxs^ev%6z5)ri$q4g4%nb^n+pr37y'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
@@ -135,5 +134,4 @@ MESSAGE_TAGS = {
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-ALLOWED_HOSTS = ['*']
 
